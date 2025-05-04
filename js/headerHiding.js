014d733d020c0ae855b3e5e77f3b875929f22b9e
Holly <hollyyhere@gmail.com>
2025-05-04 14:24:09 +0300

    footer decktop

--- js/headerHiding.js
+++ js/headerHiding.js
@@ -5,10 +5,8 @@ window.addEventListener("scroll", () => {
   const currentScrollY = window.scrollY;
 
   if (currentScrollY > lastScrollY && currentScrollY > 50) {
-    // Прокрутка вниз
     header.classList.add("hidden");
   } else {
-    // Прокрутка вверх
     header.classList.remove("hidden");
   }
 
